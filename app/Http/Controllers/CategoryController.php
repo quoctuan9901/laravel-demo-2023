6f4d6333e93554b7040aec81a0fdc158032a90d8
Kim Hoang Phuc <104184786+Hoangphuckim@users.noreply.github.com>
2023-06-08 11:12:38 +0700

    phuc category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    //
+    public function create(){
+        return view();
+    }
+
+    public function store(){
+
+    }
+
+    public function index(){
+
+    }
+
+    public function edit(){
+
+    }
+
+   public function update(){
+    
+   }
+
+    public function delete(){
+
+    }
+    
+}
